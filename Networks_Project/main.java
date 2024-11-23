b60014df21fdf98ca7c9c2a4a3fb6b8e7914a014
nozanooni <shwabushanab@gmail.com>
2024-11-24 01:10:09 +0300

    Update main.java

--- Networks_Project/main.java
+++ Networks_Project/main.java
@@ -1,3 +1,29 @@
-public class main {
+
+    public class main {
+        // Main method: Entry point for the program
+        public static void main(String[] args) {
+            // Create an instance of a class (example: MyClass) and use it
+            System.out.println("Welcome to the program!");
+    
+            // Example of calling another method
+            greetUser();
+    
+            // Example of creating an object of another class
+            MyClass myObject = new class();
+            myObject.sayHello();
+        }
+    
+        // A simple method to greet the user
+        public static void greetUser() {
+            System.out.println("Hello, User! How are you today?");
+        }
+    }
+    
+    class MyClass {
+        // Method within MyClass to demonstrate an action
+        public void sayHello() {
+            System.out.println("Hello from MyClass!");
+        }
+    }
     
 }
